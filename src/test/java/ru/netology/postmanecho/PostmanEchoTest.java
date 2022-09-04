f55bf341e407c0d9546cda58e4175147300ee327
Flynt666 <bang-kirov@bk.ru>
2022-09-04 19:00:49 +0300

    Test work

--- src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(404)
+                .statusCode(200)
                 .body("data", equalTo("Postman"))
         ;
     }
